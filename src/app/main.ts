e0c578a749bb1c06f5fe9d943a3f783b4bf95860
Gediminas Mikelenas <g.mikelenas@gmail.com>
2016-05-23 22:19:50 +0300

    Initial commit - hello world

---
+++ src/app/main.ts
@@ -0,0 +1,4 @@
+import {bootstrap}    from '@angular/platform-browser-dynamic';
+import {AppComponent} from './user-registration/user-registration.component';
+
+bootstrap(AppComponent);
---
+++ src/dist/main.js
@@ -0,0 +1,4 @@
+"use strict";
+var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
+var user_registration_component_1 = require('./user-registration/user-registration.component');
+platform_browser_dynamic_1.bootstrap(user_registration_component_1.AppComponent);
